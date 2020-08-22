c49abfca83c70ae5f709c0a889665f56294baee8
Kenta Kudo <yd37574@gmail.com>
2020-08-22 10:56:38 +0100

    Define types

--- todo.go
+++ todo.go
@@ -1,1 +1,24 @@
 package todo
+
+type Todo struct {
+	ID          string
+	Title       string
+	Description string
+	Status      TodoStatus
+}
+
+type TodoStatus int
+
+const (
+	TodoStatusCreated TodoStatus = iota
+	TodoStatusInProgress
+	TodoStatusDone
+)
+
+type TodoService interface {
+	Create(string, string) (string, error)
+	Update(Todo) error
+	Get(string) (Todo, error)
+	List() ([]Todo, error)
+	Delete(string) error
+}
